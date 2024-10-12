1daf6af6a93cb3bbe5ce125c79e7d776a4647155
Ramiro Oyhamburo <oyhamburoramiro@gmail.com>
2024-10-12 09:46:06 -0300

    Primer commit: agregar estructura inicial del proyecto

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+
+// ----------------------------VINOS---------------------------------------------
+
+// """"""""""""TINTOS"""""""""""""
+// Malbec
+// Cabernet Sauvignon
+// Cabernet Franc
+// Merlot
+
+// """"""""""""BLANCOS"""""""""""""
+// Chardonnay
+// Sauvignon Blanc
+// Torrontes
+// White Blend
+
+// """"""""""""BLANCOS"""""""""""""
+// Cabernet Franc Rose
+// Losance Rosado
+// Zorzal Terroir
+// Rose Pinot Noir
+
+// """"""""""""BLANCOS"""""""""""""
+// Brut Nature
+// Extra Brut
+// Dulce
+
+// ----------------------------PROMOCIONES----------------------------------------
+// """"""""""""Mercadopago"""'""""""""""
+// 3 cuotas sin interés
+
+// """"""""""""ENVIO""""""""""""""""""""
+// Envio gratis, cuota minima de 42mil
+
+// """"""""""""Día de la madre"""""""""
+//llevando más de dos botellas, te regalamos una de regalo.
+
+// """""""""""""Cupón de descuento""""""""
+// Poner una palabra y que se aplique el 20% de descuento.
+
+
+
+
