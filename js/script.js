ff8b485bb156c03a31b60a303f67663484b9022a
Ramiro Oyhamburo <oyhamburoramiro@gmail.com>
2024-10-19 13:55:31 -0300

    Actualización de index.html y script.js, eliminación de prueba.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ const vinos = [
 ];
 
 
-
 // En este array guardamos el pedido del cliente
 let pedidosSolicitados =[];
 
@@ -17,33 +16,38 @@ let pedidosSolicitados =[];
 // Funcion que llama a todas las demas funciones
 function iniciar (){
 
-elegirVino() // Llamamos a la función para que el usuario elija los vinos
+const quieroComprar = confirm("¿Quieres comprar un producto?");
+if(quieroComprar){
+  elegirVino() // Llamamos a la función para que el usuario elija los vinos
+}else{
+  return;
+}
 
-let total = sumaDePedidos (pedidosSolicitados); // Luego de elegir los vinos, llamamos a la función que suma los precios, la guardamos en una variable y le pasamos un argumento real.
+const total = sumaDePedidos (pedidosSolicitados); // Luego de elegir los vinos, llamamos a la función que suma los precios, la guardamos en una variable y le pasamos un argumento real.
 alert(`El total de tu pedido es de $${total}`);
 
 const quieroDescuento = confirm("¿Tienes un cupón de descuento?");
 if(quieroDescuento){
-    let cupones = prompt("Escribi tu cupón");
-    let precioFinalConDescuento = calcularDescuentos(total, cupones);
-    alert(`Tu precio final con descuento es de $${precioFinalConDescuento}`);
+    const cupones = prompt("Escribi tu cupón").toLocaleLowerCase();
+    const precioFinalConDescuento = calcularDescuentos(total, cupones);
+    alert(`Tu precio final es de $${precioFinalConDescuento}`);
     alert(`Gracias por tu compra.`);
-}else{
-    alert("Gracias por tu compra");
 }
+
+
 }
 
 
 // Función para elegir el tipo de vino y cantidad de botellas.
 function elegirVino(){
-    let deseaPedirDenuevo = true; // Valor verdadero para comenzar el ciclo while.
+  let deseaPedirDenuevo = true; // Valor verdadero para comenzar el ciclo while.
     
     while(deseaPedirDenuevo){
-    let tipoDeVino = parseInt(prompt("Elige un tipo de vino: \n 1) Vino Tinto $5000\n 2) Vino Blanco $4000 \n 3) Vino Rosado $4500"));
-    let cantidadDeBotellas = parseInt(prompt("Coloque la cantidad de botellas:"));
+    const tipoDeVino = parseInt(prompt("Elija un número del 1 al 3 para seleccionar su vino: \n 1) Vino Tinto $5000\n 2) Vino Blanco $4000 \n 3) Vino Rosado $4500"));
+    const cantidadDeBotellas = parseInt(prompt("Coloque la cantidad de botellas:"));
 
-    if (tipoDeVino >= 1 && tipoDeVino <= 3){
-      let elegirDeLaLista = vinos[tipoDeVino -1];   // Aquí elijo el elemento de la lista de arrays y le resto 1, ya que comienzan en 0.
+    if ( tipoDeVino >= 1 && tipoDeVino <= 3){
+      const elegirDeLaLista = vinos[tipoDeVino -1];   // Aquí elijo el elemento de la lista de arrays y le resto 1, ya que comienzan en 0.
       
       pedidosSolicitados.push({ //Aquí estoy mandando los elementos de la lista de vinos finamente seleccionados, a un array donde guardo todo.
         nombre: elegirDeLaLista.nombre,
@@ -52,12 +56,14 @@ function elegirVino(){
       });
       
       alert(`Has seleccionado ${cantidadDeBotellas} botellas de ${elegirDeLaLista.nombre} por un valor de $${elegirDeLaLista.precio * cantidadDeBotellas}`);  
-    } else{
-      alert("Coloque un número válido, del 1 al 3");
+    } 
+
+    if (isNaN(tipoDeVino) || tipoDeVino < 1 || tipoDeVino > 3){
+      tipoDeVino = alert("Debes colocar un número valido");
     }
+  
     deseaPedirDenuevo = confirm("Quieres hacer otro pedido?")  // Aquí si lo deseo transformo el ciclo en falso, para que termine.
     }
-
 }
 
 
@@ -73,14 +79,13 @@ return suma;
 
 
 // Función para aplicar descuentos
-function calcularDescuentos(ejemploUno, ejemploDos){ 
-    let descuento = 0;
+function calcularDescuentos(ejemploUno, ejemploDos){  //Utilize la palabra "ejemplo@" para guiarme y trabajar el tema de argumentos y propiedades
    if (ejemploDos === "mendoza2025"){
-    descuento = ejemploUno - ejemploUno * 0.1;
+    return  ejemploUno - ejemploUno * 0.1;
    } else{
     alert("El codigo no existe")
    }
-   return descuento;
+   return ejemploUno;
 }
 
 
